7eb06393c20141526fd199364bf63225ade77a2d
Velmurugan Ambalam <v.a.velmurugan@gmail.com>
2026-05-16 23:14:42 +0530

    initial commit

---
+++ src/main/java/com/kafkaexample/ProductMicroserviceForKafka/ProductMicroserviceForKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaexample.ProductMicroserviceForKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceForKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceForKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaexample/ProductMicroserviceForKafka/ProductMicroserviceForKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaexample.ProductMicroserviceForKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceForKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
